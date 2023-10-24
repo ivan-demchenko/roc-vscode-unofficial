4446aea1779f277e2726158d14625bd65679f169
Ivan Demchenko <raqy.style@gmail.com>
2023-10-24 15:51:15 +0200

    chore: fix release config

--- release.config.cjs
+++ release.config.cjs
@@ -5,8 +5,20 @@ module.exports = {
   branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
-    '@semantic-release/changelog',
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+        tarballDir: false,
+      },
+    ],
     [
       'semantic-release-vsce',
       {
