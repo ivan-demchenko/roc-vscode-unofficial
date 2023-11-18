673c2107b82acf366ca4ff0faa1526d971331dee
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-11-17 22:47:54 -0500

    chore: add roc as input

--- flake.nix
+++ flake.nix
@@ -2,23 +2,22 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    roc = {
+      url = "github:roc-lang/roc";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
         node = pkgs.nodejs_18;
-      in
-      {
+        roc-full = inputs.roc.packages.${system}.full;
+      in {
         formatter = pkgs.nixpkgs-fmt;
         devShells = {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs;
-              [
-                node
-              ];
-          };
+          default = pkgs.mkShell { buildInputs = [ node roc-full ]; };
         };
       });
 }
